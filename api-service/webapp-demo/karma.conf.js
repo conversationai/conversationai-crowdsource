3296b8702ff292fc5cb0d65cc8b752f8b682b4ff
Rachel Rosen <rachelrosen@google.com>
2018-03-27 11:57:54 -0400

    More troubleshooting test disconnect error in travis ci tests.

--- api-service/webapp-demo/karma.conf.js
+++ api-service/webapp-demo/karma.conf.js
@@ -29,6 +29,7 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
+    browserDisconnectTolerance: 2,
     browserNoActivityTimeout: 50000,
     customLaunchers: {
       Chrome_travis_ci: {
