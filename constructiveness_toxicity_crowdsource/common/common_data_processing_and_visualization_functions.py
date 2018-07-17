6fb0ab120a1d94c08231e6faebf690691c1487cf
Varada <kvarada@gmail.com>
2018-07-17 12:45:33 -0700

    cleaning code and adding comments

--- constructiveness_toxicity_crowdsource/common/common_data_processing_and_visualization_functions.py
+++ constructiveness_toxicity_crowdsource/common/common_data_processing_and_visualization_functions.py
@@ -2,11 +2,29 @@ import matplotlib.pyplot as plt
 
 # create and save plots
 def create_plot(size, labels, colors, title = 'Test', dpi = 300):
+    '''    
+    :param size: (list of floats) Sizes for each slice in the donut plot 
+    :param labels: (list of strs) Labels for each slice in the donut plot
+    :param colors: (list of strs) Colour names for each slice in the donut plot
+    :param title: (str) The title of the plot. The default value is 'Test'
+    :param dpi: (int) The resolution for the plot. The default value is 300
+    :return: None 
+    Description: Given size, labels, colors, title, and dpi, this function
+    plots a shows a donut plot. 
+    
+    Examples: 
+    >>> size = [44.0, 44.0, 12.0]
+    >>> labels = 'Constructive', 'Non constructive', 'Not sure'
+    >>> colors = ['xkcd:green', 'xkcd:red', 'xkcd:orange']
+    >>> Title = 'Constructiveness distribution'
+    >>> create_plot(size, labels, colors, title)
+    '''     
     plt.gcf().clear()
+    
     # Create a circle for the center of the plot
     patches, texts, autotexts  = plt.pie(size, labels = labels, autopct='%.1f%%', colors = colors)
-    #plt.legend(patches, labels, loc="best")
     p = plt.gcf()
+    
     # Create a circle for the center of the plot
     my_circle=plt.Circle( (0,0), 0.7, color='white')
     p.gca().add_artist(my_circle)
@@ -18,6 +36,10 @@ def create_plot(size, labels, colors, title = 'Test', dpi = 300):
     #print('Plot saved at the location: ', save_file_name)
     
 def nominalize_constructiveness(constructiveness_score):
+    '''
+    :constructiveness_score: (float) The constructiveness score for nominalizing
+    :return: (str) The nominalized constructiveness label
+    '''
     try: 
         if constructiveness_score > 0.6:
             return 'yes'
@@ -30,6 +52,10 @@ def nominalize_constructiveness(constructiveness_score):
         return np.nan    
     
 def nominalize_toxicity(toxicity_value):
+    '''
+    :toxicity_value: (int) The toxicity level for nominalizing
+    :return: (str) The nominalized toxicity level
+    '''    
     try: 
         if toxicity_value > 3:
             return 'Very toxic'
