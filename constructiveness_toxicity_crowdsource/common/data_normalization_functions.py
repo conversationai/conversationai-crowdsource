25491304286b1a80c98df0a26ff6296676b4c8f7
Varada <kvarada@gmail.com>
2018-07-19 16:31:14 -0700

    adding binarize_toxicity_with_multiple_answers function and defining constants

--- constructiveness_toxicity_crowdsource/common/data_normalization_functions.py
+++ constructiveness_toxicity_crowdsource/common/data_normalization_functions.py
@@ -1,5 +1,7 @@
 import numpy as np
-    
+TOXICITY_LEVEL_THRESHOLD = 1.8
+CONSTRUCTIVENESS_SCORE_THRESHOLD = 0.6
+
 def nominalize_constructiveness(constructiveness_score):
     '''
     :constructiveness_score: (float) The constructiveness score for nominalizing
@@ -15,6 +17,7 @@ def nominalize_constructiveness(constructiveness_score):
     except:
         print("Error in the input: ", constructiveness_score)
         return np.nan
+    
     
 def nominalize_toxicity(toxicity_value):
     '''
@@ -34,6 +37,7 @@ def nominalize_toxicity(toxicity_value):
         print("Error in the input: ", toxicity_value)
         return np.nan
 
+    
 def binarize_toxicity(toxicity_level):    
     '''
     :toxicity_level: (float) the toxicity level to be binarized
@@ -44,5 +48,38 @@ def binarize_toxicity(toxicity_level):
     it is considered as a toxic comment else it is considered as 
     a non-toxic comment. 
     '''
-    return 'yes' if toxicity_level >= 1.8 else 'no'
-           
+    return 'yes' if float(toxicity_level) >= TOXICITY_LEVEL_THRESHOLD else 'no'
+
+
+def binarize_toxicity_with_multiple_answers(toxicity_level):    
+    '''
+    :toxicity_level: (float/str) the toxicity level to be binarized
+    :return: (str) binarized toxicity level 
+    
+    Given the toxicity_level, this function returns a binary 
+    representation of the toxicity level. If toxicity_level > TOXICITY_LEVEL_THRESHOLD, 
+    it is considered as a toxic comment else it is considered as 
+    a non-toxic comment. 
+    '''
+    if type(toxicity_level) == str:
+        toxicity = toxicity_level.split()
+        if ('2' in toxicity or '3' in toxicity or '4' in toxicity):
+            return 'yes'
+        else:
+            return 'no'
+        
+    return 'yes' if float(toxicity_level) >= TOXICITY_LEVEL_THRESHOLD else 'no'
+
+
+def binarize_constructiveness(constructiveness_score):    
+    '''
+    :constructiveness_score: (float) the constructiveness_score to be binarized
+    :return: (str) binarized constructiveness 
+    
+    Given the constructiveness_score, this function returns a binary 
+    representation of the constructiveness_score. If constructiveness_score > 
+    CONSTRUCTIVENESS_SCORE_THRESHOLD, it is considered as a constructive comment 
+    else it is considered as a non-constructive comment. 
+    '''
+    return 'yes' if constructiveness_score >= CONSTRUCTIVENESS_SCORE_THRESHOLD else 'no'
+
