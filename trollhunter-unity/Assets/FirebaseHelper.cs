a7a0ecb6d9a3ffe53a258998434c7da08ab218ce
Noah Wang <nfw@google.com>
2018-09-18 15:56:17 -0700

    Add FirebaseHelper and UserAccount stubs

---
+++ trollhunter-unity/Assets/FirebaseHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+// Helper class to handle Firebase-related functionalities.
+public class FirebaseHelper {
+
+	private UserAccount userAccount;
+	private List<string> textSnapshot;
+
+	// This should initialize the connection to Firebase.
+	public void Init(UserAccount userAccount, Callback callback) {
+		this.userAccount = userAccount;
+		callback.OnSuccess();
+	}
+
+	public interface Callback {
+		void OnSuccess();
+		void OnFailure();
+	}
+
+	public void SubmitText(string text, Callback callback) {
+		textSnapshot.Add(text);
+		callback.OnSuccess();
+	}
+
+	public List<string> GetTextSnapshot() {
+		return textSnapshot;
+	}
+}
---
+++ trollhunter-unity/Assets/UserAccount.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+// Stub class to hold user account information.
+public class UserAccount {
+	public readonly string GoogleAccountName;
+
+	public UserAccount(string googleAccount) {
+		GoogleAccountName = googleAccount;
+	}
+}
