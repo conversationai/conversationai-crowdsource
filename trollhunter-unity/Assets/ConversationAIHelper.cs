cabe39696872383f4980ecea6a7a0a4bfb03daf6
Noah Wang <nfw@google.com>
2018-09-18 14:53:09 -0700

    Adding classes serving as interface to ConversationAI

---
+++ trollhunter-unity/Assets/ConversationAIHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Helper class that connects to Conversation AI.
+public class ConversationAIHelper {
+	System.Random random = new System.Random();
+
+	public interface Callback {
+		void score(Score score);
+	}
+
+	// Query the ConversationAI.
+	public bool query(string text, Callback callback) {
+		// Stub
+		Score score = getDummyScore(text);
+		callback.score(score);
+		return true;
+	}
+
+	private Score getDummyScore(string text) {
+		Score score = new Score();
+		score.toxicity = 1.0F / (float) text.Length;
+		score.severeToxicity = score.toxicity / 2.0F;
+		score.inflammatory = (float) random.NextDouble();
+
+		return score;
+	}
+}
---
+++ trollhunter-unity/Assets/Score.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Stores toxicity scores returned from Conversation AI.
+public class Score {
+	public float toxicity;
+	public float severeToxicity;
+	public float inflammatory;
+}
